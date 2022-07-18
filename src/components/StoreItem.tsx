4686f4fab8b6e66cf554a50ba78eef594d1ffc36
azatseyrek <azatseyrek@hotmail.com>
2022-07-18 11:32:47 +0300

    context created

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import {Card} from 'react-bootstrap';
+import {Button, Card} from 'react-bootstrap';
+import {useShoppingCart} from '../context/ShoppingCartContext';
 import {formatCurrency} from '../utilities/formatCurrency';
 
 type StoreItemProps = {
@@ -9,8 +10,15 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({id, name, imageUrl, price}: StoreItemProps) => {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+  const quantity = getItemQuantity(id);
   return (
-    <Card className="bg-warning" style={{borderRadius: '8px'}}>
+    <Card className="bg-warning h-100" style={{borderRadius: '8px'}}>
       <Card.Img
         variant="top"
         src={imageUrl}
@@ -22,6 +30,44 @@ const StoreItem = ({id, name, imageUrl, price}: StoreItemProps) => {
           <span>{name}</span>
           <span className=" ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button
+              onClick={() => increaseCartQuantity(id)}
+              className="w-100 bg-dark border-0"
+            >
+              {' '}
+              + Add to Cart
+            </Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{gap: '.5rem'}}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{gap: '.5rem'}}
+              >
+                <Button onClick={() => decreaseCartQuantity(id)} variant="dark">
+                  -
+                </Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button onClick={() => increaseCartQuantity(id)} variant="dark">
+                  +
+                </Button>
+              </div>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={() => removeFromCart(id)}
+              >
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
